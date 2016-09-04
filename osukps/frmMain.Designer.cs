b763a5106aa2cb59278f53182cf2f9233a94a894
dnjstlr555 <ssplas@naver.com>
2016-09-04 15:39:58 +0900

    Add files via upload
    KpsHandler, frmMain

--- osukps/frmMain.Designer.cs
+++ osukps/frmMain.Designer.cs
@@ -28,146 +28,201 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.pnlKeys = new System.Windows.Forms.Panel();
-			this.pnlInfo = new System.Windows.Forms.Panel();
-			this.lblTotal = new System.Windows.Forms.Label();
-			this.lblKps = new System.Windows.Forms.Label();
-			this.cms = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.tsiAddButton = new System.Windows.Forms.ToolStripMenuItem();
-			this.tsiRemoveButton = new System.Windows.Forms.ToolStripMenuItem();
-			this.tsiReset = new System.Windows.Forms.ToolStripMenuItem();
-			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-			this.tsiExit = new System.Windows.Forms.ToolStripMenuItem();
-			this.tmrProcess = new System.Windows.Forms.Timer(this.components);
-			this.pnlInfo.SuspendLayout();
-			this.cms.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// pnlKeys
-			// 
-			this.pnlKeys.AutoSize = true;
-			this.pnlKeys.BackColor = System.Drawing.Color.Black;
-			this.pnlKeys.Dock = System.Windows.Forms.DockStyle.Left;
-			this.pnlKeys.Location = new System.Drawing.Point(0, 0);
-			this.pnlKeys.Name = "pnlKeys";
-			this.pnlKeys.Size = new System.Drawing.Size(0, 36);
-			this.pnlKeys.TabIndex = 0;
-			// 
-			// pnlInfo
-			// 
-			this.pnlInfo.AutoSize = true;
-			this.pnlInfo.BackColor = System.Drawing.Color.Black;
-			this.pnlInfo.Controls.Add(this.lblTotal);
-			this.pnlInfo.Controls.Add(this.lblKps);
-			this.pnlInfo.Dock = System.Windows.Forms.DockStyle.Left;
-			this.pnlInfo.Location = new System.Drawing.Point(0, 0);
-			this.pnlInfo.MinimumSize = new System.Drawing.Size(55, 36);
-			this.pnlInfo.Name = "pnlInfo";
-			this.pnlInfo.Size = new System.Drawing.Size(55, 36);
-			this.pnlInfo.TabIndex = 1;
-			// 
-			// lblTotal
-			// 
-			this.lblTotal.AutoSize = true;
-			this.lblTotal.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblTotal.ForeColor = System.Drawing.Color.White;
-			this.lblTotal.Location = new System.Drawing.Point(0, 18);
-			this.lblTotal.MinimumSize = new System.Drawing.Size(0, 18);
-			this.lblTotal.Name = "lblTotal";
-			this.lblTotal.Size = new System.Drawing.Size(16, 18);
-			this.lblTotal.TabIndex = 1;
-			this.lblTotal.Text = "0";
-			this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
-			// lblKps
-			// 
-			this.lblKps.AutoSize = true;
-			this.lblKps.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblKps.ForeColor = System.Drawing.Color.White;
-			this.lblKps.Location = new System.Drawing.Point(0, 0);
-			this.lblKps.MinimumSize = new System.Drawing.Size(0, 18);
-			this.lblKps.Name = "lblKps";
-			this.lblKps.Size = new System.Drawing.Size(42, 18);
-			this.lblKps.TabIndex = 0;
-			this.lblKps.Text = "0 kps";
-			this.lblKps.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
-			// cms
-			// 
-			this.cms.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.components = new System.ComponentModel.Container();
+            this.pnlKeys = new System.Windows.Forms.Panel();
+            this.pnlInfo = new System.Windows.Forms.Panel();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblKps = new System.Windows.Forms.Label();
+            this.cms = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsiAddButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsiRemoveButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveKeySettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsiReset = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.creditsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yugecinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dnjstlr555ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmrProcess = new System.Windows.Forms.Timer(this.components);
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlInfo.SuspendLayout();
+            this.cms.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlKeys
+            // 
+            this.pnlKeys.AutoSize = true;
+            this.pnlKeys.BackColor = System.Drawing.Color.Black;
+            this.pnlKeys.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlKeys.Location = new System.Drawing.Point(0, 0);
+            this.pnlKeys.Name = "pnlKeys";
+            this.pnlKeys.Size = new System.Drawing.Size(0, 33);
+            this.pnlKeys.TabIndex = 0;
+            // 
+            // pnlInfo
+            // 
+            this.pnlInfo.AutoSize = true;
+            this.pnlInfo.BackColor = System.Drawing.Color.Black;
+            this.pnlInfo.Controls.Add(this.lblTotal);
+            this.pnlInfo.Controls.Add(this.lblKps);
+            this.pnlInfo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlInfo.Location = new System.Drawing.Point(0, 0);
+            this.pnlInfo.MinimumSize = new System.Drawing.Size(64, 33);
+            this.pnlInfo.Name = "pnlInfo";
+            this.pnlInfo.Size = new System.Drawing.Size(64, 33);
+            this.pnlInfo.TabIndex = 1;
+            this.pnlInfo.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlInfo_Paint);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.White;
+            this.lblTotal.Location = new System.Drawing.Point(0, 17);
+            this.lblTotal.MinimumSize = new System.Drawing.Size(0, 17);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(16, 17);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "0";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblKps
+            // 
+            this.lblKps.AutoSize = true;
+            this.lblKps.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKps.ForeColor = System.Drawing.Color.White;
+            this.lblKps.Location = new System.Drawing.Point(0, 0);
+            this.lblKps.MinimumSize = new System.Drawing.Size(0, 17);
+            this.lblKps.Name = "lblKps";
+            this.lblKps.Size = new System.Drawing.Size(42, 17);
+            this.lblKps.TabIndex = 0;
+            this.lblKps.Text = "0 kps";
+            this.lblKps.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // cms
+            // 
+            this.cms.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsiAddButton,
             this.tsiRemoveButton,
+            this.saveKeySettingsToolStripMenuItem,
             this.tsiReset,
+            this.resetToolStripMenuItem,
+            this.settingsToolStripMenuItem,
             this.toolStripSeparator1,
+            this.creditsToolStripMenuItem,
             this.tsiExit});
-			this.cms.Name = "cms";
-			this.cms.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-			this.cms.ShowImageMargin = false;
-			this.cms.Size = new System.Drawing.Size(128, 98);
-			// 
-			// tsiAddButton
-			// 
-			this.tsiAddButton.Name = "tsiAddButton";
-			this.tsiAddButton.Size = new System.Drawing.Size(127, 22);
-			this.tsiAddButton.Text = "Add button";
-			this.tsiAddButton.Click += new System.EventHandler(this.tsiAddButton_Click);
-			// 
-			// tsiRemoveButton
-			// 
-			this.tsiRemoveButton.Name = "tsiRemoveButton";
-			this.tsiRemoveButton.Size = new System.Drawing.Size(127, 22);
-			this.tsiRemoveButton.Text = "Remove button";
-			this.tsiRemoveButton.Click += new System.EventHandler(this.tsiRemoveButton_Click);
-			// 
-			// tsiReset
-			// 
-			this.tsiReset.Name = "tsiReset";
-			this.tsiReset.Size = new System.Drawing.Size(127, 22);
-			this.tsiReset.Text = "Reset total keys";
-			this.tsiReset.Click += new System.EventHandler(this.tsiReset_Click);
-			// 
-			// toolStripSeparator1
-			// 
-			this.toolStripSeparator1.Name = "toolStripSeparator1";
-			this.toolStripSeparator1.Size = new System.Drawing.Size(124, 6);
-			// 
-			// tsiExit
-			// 
-			this.tsiExit.Name = "tsiExit";
-			this.tsiExit.Size = new System.Drawing.Size(127, 22);
-			this.tsiExit.Text = "&Exit";
-			this.tsiExit.Click += new System.EventHandler(this.tsiExit_Click);
-			// 
-			// tmrProcess
-			// 
-			this.tmrProcess.Enabled = true;
-			this.tmrProcess.Interval = 5;
-			this.tmrProcess.Tick += new System.EventHandler(this.tmrProcess_Tick);
-			// 
-			// frmMain
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.AutoSize = true;
-			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.BackColor = System.Drawing.Color.Magenta;
-			this.ClientSize = new System.Drawing.Size(139, 36);
-			this.ContextMenuStrip = this.cms;
-			this.Controls.Add(this.pnlInfo);
-			this.Controls.Add(this.pnlKeys);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.MaximizeBox = false;
-			this.MinimumSize = new System.Drawing.Size(0, 36);
-			this.Name = "frmMain";
-			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "frmMain";
-			this.pnlInfo.ResumeLayout(false);
-			this.pnlInfo.PerformLayout();
-			this.cms.ResumeLayout(false);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.cms.Name = "cms";
+            this.cms.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.cms.ShowImageMargin = false;
+            this.cms.Size = new System.Drawing.Size(133, 208);
+            this.cms.Opening += new System.ComponentModel.CancelEventHandler(this.cms_Opening);
+            // 
+            // tsiAddButton
+            // 
+            this.tsiAddButton.Name = "tsiAddButton";
+            this.tsiAddButton.Size = new System.Drawing.Size(132, 22);
+            this.tsiAddButton.Text = "Add button";
+            this.tsiAddButton.Click += new System.EventHandler(this.tsiAddButton_Click);
+            // 
+            // tsiRemoveButton
+            // 
+            this.tsiRemoveButton.Name = "tsiRemoveButton";
+            this.tsiRemoveButton.Size = new System.Drawing.Size(132, 22);
+            this.tsiRemoveButton.Text = "Remove button";
+            this.tsiRemoveButton.Click += new System.EventHandler(this.tsiRemoveButton_Click);
+            // 
+            // saveKeySettingsToolStripMenuItem
+            // 
+            this.saveKeySettingsToolStripMenuItem.Name = "saveKeySettingsToolStripMenuItem";
+            this.saveKeySettingsToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.saveKeySettingsToolStripMenuItem.Text = "Save Key Setup";
+            this.saveKeySettingsToolStripMenuItem.Click += new System.EventHandler(this.saveKeySettingsToolStripMenuItem_Click);
+            // 
+            // tsiReset
+            // 
+            this.tsiReset.Name = "tsiReset";
+            this.tsiReset.Size = new System.Drawing.Size(132, 22);
+            this.tsiReset.Text = "Reset total keys";
+            this.tsiReset.Click += new System.EventHandler(this.tsiReset_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.resetToolStripMenuItem.Text = "Reset max kps";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(129, 6);
+            // 
+            // creditsToolStripMenuItem
+            // 
+            this.creditsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yugecinToolStripMenuItem,
+            this.dnjstlr555ToolStripMenuItem});
+            this.creditsToolStripMenuItem.Name = "creditsToolStripMenuItem";
+            this.creditsToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.creditsToolStripMenuItem.Text = "Credits";
+            // 
+            // yugecinToolStripMenuItem
+            // 
+            this.yugecinToolStripMenuItem.Name = "yugecinToolStripMenuItem";
+            this.yugecinToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.yugecinToolStripMenuItem.Text = "yugecin";
+            // 
+            // dnjstlr555ToolStripMenuItem
+            // 
+            this.dnjstlr555ToolStripMenuItem.Name = "dnjstlr555ToolStripMenuItem";
+            this.dnjstlr555ToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.dnjstlr555ToolStripMenuItem.Text = "dnjstlr555";
+            // 
+            // tsiExit
+            // 
+            this.tsiExit.Name = "tsiExit";
+            this.tsiExit.Size = new System.Drawing.Size(132, 22);
+            this.tsiExit.Text = "&Exit";
+            this.tsiExit.Click += new System.EventHandler(this.tsiExit_Click);
+            // 
+            // tmrProcess
+            // 
+            this.tmrProcess.Enabled = true;
+            this.tmrProcess.Interval = 5;
+            this.tmrProcess.Tick += new System.EventHandler(this.tmrProcess_Tick);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Magenta;
+            this.ClientSize = new System.Drawing.Size(162, 33);
+            this.ContextMenuStrip = this.cms;
+            this.Controls.Add(this.pnlInfo);
+            this.Controls.Add(this.pnlKeys);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(0, 33);
+            this.Name = "frmMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMain";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.pnlInfo.ResumeLayout(false);
+            this.pnlInfo.PerformLayout();
+            this.cms.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -184,5 +239,11 @@
 		private System.Windows.Forms.Timer tmrProcess;
 		private System.Windows.Forms.ToolStripMenuItem tsiAddButton;
 		private System.Windows.Forms.ToolStripMenuItem tsiRemoveButton;
-	}
+        private System.Windows.Forms.ToolStripMenuItem creditsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yugecinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveKeySettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dnjstlr555ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+    }
 }
--- osukps/frmMain.cs
+++ osukps/frmMain.cs
@@ -7,14 +7,12 @@ namespace osukps
 {
 	public partial class frmMain : Form
 	{
-
 		private const byte MAX_BUTTONS = 10;
 		private const byte INITIAL_BUTTONS = 4;
-
+        public int[] key;
 		private KpsHandler kpsHandler;
 		private KpsButton[] btns;
 		private byte buttonCount;
-
 		public frmMain()
 		{
 			InitializeComponent();
@@ -39,9 +37,13 @@ namespace osukps
 			}
 			SetButtonCount( INITIAL_BUTTONS );
 		}
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, int size, string filePath);
 
 #region dragcodez
-		private bool moveForm;
+        private bool moveForm;
 		private Point moveOffset;
 
 		private void f_MouseDown( object sender, MouseEventArgs e )
@@ -62,8 +64,11 @@ namespace osukps
 				this.Location = new Point( this.Location.X + ( e.Location.X - moveOffset.X ), this.Location.Y + ( e.Location.Y - moveOffset.Y ) );
 			}
 		}
-#endregion
-
+        #endregion
+        public int kpsmax(int max)
+        {
+            return 1;
+        }
 		private void tmrProcess_Tick( object sender, EventArgs e )
 		{
 			byte keyCount = 0;
@@ -105,5 +110,39 @@ namespace osukps
 			kpsHandler.ResetTotal();
 		}
 
-	}
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cms_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void saveKeySettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            WritePrivateProfileString("Count", "count", buttonCount.ToString(), "./setting.ini");
+            for (var i = 0; i < buttonCount; i++)
+            {
+                WritePrivateProfileString("INIT_PATH", "key" + (i+1), "Testing" , "./setting.ini");
+            }
+        }
+
+        private void pnlInfo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tmrMax_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            kpsHandler.resetmax();
+        }
+    }
 }
